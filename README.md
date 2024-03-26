176afe284539634007d94d4c79cd062b2626c64f
MalazHassan <TheFoodanese@users.noreply.github.com>
2024-03-26 15:07:57 +0000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# undefined
+# lorem ipsum dolor sit amet
+        
+        # Description
+        lorem ipsum dolor sit amet
 
+        # Installation
+        lorem ipsum dolor sit amet
+
+        # Usage
+        undefined
+
+        # How To Contribute
+        lorem ipsum dolor sit amet
+
+        # Tests
+        lorem ipsum dolor sit amet
+
+        # Contact-Info
+        For any questions you can refer to [github.com/TheFoodanese] or malazhassan@yahoo.co.uk
+        
--- index.js
+++ index.js
@@ -4,88 +4,101 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [{
-    type: 'input',
-    name: 'Title of Project',
-    message: "Enter The Title Project Title",
-  },
-  {
-    type: 'input',
-    name: 'Description',
-    message: "Write a Description For Your Project",
-  },
-  {
-    type: 'input',
-    name: 'Table of Contents',
-    message: "Write A Table of Contents",
-  },
-  {
-    type: 'input',
-    name: 'Installation',
-    message: "Describe The Installation Process",
-  },
-  {
-    type: 'input',
-    name: 'Usage',
-    message: "Explain How To Use Your Application",
-  },
-  {
-    type: 'input',
-    name: 'License',
-    message: "Provide A License For Your Project",
-  },
-  {
-    type: 'input',
-    name: 'Contributing',
-    message: "Add Collaborators/Contributors/Contributor.md",
-  },
-  {
-    type: 'input',
-    name: 'Tests',
-    message: "Describe How To Run Testing",
-  },
-  {
-    type: 'input',
-    name: 'Questions',
-    message: "Where Can Users Contact You",
-  },
-
+const questions = [
+    
 ];
 
-
+const readMEQs = (question) => {
+    for (let i = 0; i < questions.length; i++) {
+        return question[i];
+      }
+    }
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("README.md created successfully!");
-        }
-      });
+    
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the Project Title?: ',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Tell Us About Your Project? ',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How to Install Application: ',
+        },
+        {
+            type: 'input',
+            name: 'guidelines',
+            message: 'What Are the Guidelines On How to Use Your Application?: ',
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Add Contributors/Collaborators: ',
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Test Your Code : ',
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Select Project License Type",
+            choices: ['AFL-3.0','Apache','Artistic','BSL','BSD','ECL','EPL','EUPL','AGPL','GPL','LGPL','ISC','LPPL','MS-PL','MPL','OSL','PostgreSQL','OFL','NCSA','Unlicense','Zlib']
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter Github Username: ',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Email: ',
+        },
 
-    inquirer
-    .prompt(questions)
-    .then((Answers) => {
-      console.log("Answers:", Answers);
-      
-    // Generate markdown
-    const markdown = generateMarkdown(Answers);
+    ])
+    .then((answers) => {
+        const readMeTemplate = 
+        `# ${answers.title}
+        
+        # Description
+        ${answers.description}
 
-    // Add markdown info to readme file
-    writeToFile("README.md", markdown);
-  })
-  .catch((err) => {
-    console.error("Error generating README: ", err);
-  });
+        # Installation
+        ${answers.installation}
+
+        # Usage
+        ${answers.guidlines}
+
+        # How To Contribute
+        ${answers.contributions}
+
+        # Tests
+        ${answers.testing}
+
+        # Contact-Info
+        For any questions you can refer to [github.com/${answers.username}] or ${answers.email}
+        `
+        fs.writeFile('README.md' , readMeTemplate, (err) => {
+            console.log(err);
+        })
+    }
+
+    )
 }
 
 // function call to initialize program
 init();
-
-
-
